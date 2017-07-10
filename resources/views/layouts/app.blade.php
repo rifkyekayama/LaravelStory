c4402c2463db39ba2c73e61e877ea241308257af
krisnaw <krisna.w2010@gmail.com>
2017-07-10 11:44:38 +0800

    Fixing Navigation Blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -80,23 +80,7 @@
                     </div>
                 @else
                     <div class="col-md-4">
-                        <div class="panel panel-default">
-                            <div class="panel-body">
-                                <ul class="nav nav-pills nav-stacked">
-                                    <li class="{{ Request::is('home') ? 'active' : '' }}">
-                                        <a href="/home">Dasboard</a>
-                                    </li>
-                                    <li class="{{ Request::is('google/maps') ? 'active' : '' }}">
-                                        <a href="/google/maps">Google Maps</a>
-                                        <ul class="nav nav-pills nav-stacked">
-                                            <li>
-                                                <a href="">Basic Google Maps</a>
-                                            </li>
-                                        </ul>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
+                        @include('layouts.navigation')
                     </div>
                     <div class="col-md-8">
                         @yield('content')
