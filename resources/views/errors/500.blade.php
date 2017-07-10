926be79a309aef3c76345641ef1df31f31e56789
krisnaw <krisna.w2010@gmail.com>
2017-07-10 11:39:29 +0800

    Fixing Sentry DSN

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,6 +1,7 @@
 <div class="content">
     <div class="title">Something went wrong.</div>
 @unless(empty($sentryID))
+
     <!-- Sentry JS SDK 2.1.+ required -->
         <script src="https://cdn.ravenjs.com/3.3.0/raven.min.js"></script>
 
@@ -9,7 +10,7 @@
                 eventId: '{{ $sentryID }}',
 
                 // use the public DSN (dont include your secret!)
-
+                dsn: '{{env('SENTRY_DSN')}}'
             });
         </script>
     @endunless
